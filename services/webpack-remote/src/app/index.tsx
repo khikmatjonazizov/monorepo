63b95e18a59eee7749c5538e644942f2e07fa566
khikmatjon <azizovxikmatjon04@gmail.com>
2024-08-14 11:08:13 +0500

    migrated from npm to pnpm

--- services/webpack-remote/src/app/index.tsx
+++ services/webpack-remote/src/app/index.tsx
@@ -1,5 +1,10 @@
+import { Button } from "../shared/ui/button"
+
 export const App: React.FC = () => {
   return (
-    <h1>Remote</h1>
+    <div>
+      <h1>Remote</h1>
+      <Button />
+    </div>
   )
 }
